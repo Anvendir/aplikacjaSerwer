763f24542f179c53c7f174daeea59fa8478fe8f8
Rafal Kobak <rafal90gg@gmail.com>
2015-12-29 19:55:58 +0100

    Rozdzial 5 dodano klienta

--- chapter5/wrappers.cpp
+++ chapter5/wrappers.cpp
@@ -8,10 +8,14 @@
 #include <cstring>
 
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define MAXLINE 4096 /* max text line length */
 
 void err_sys(const char *fmt, ...);
+void err_quit(const char *fmt, ...);
+char* Fgets(char *ptr, int n, FILE *stream);
+void Fputs(const char *ptr, FILE *stream);
 
 /* Write "n" bytes to a descriptor. */
 ssize_t writen(int fd, const void *vptr, size_t n)
@@ -43,6 +47,83 @@ void Writen(int fd, void *ptr, size_t nbytes)
 {
     if (writen(fd, ptr, nbytes) != nbytes)
         err_sys("writen error");
+}
+
+static ssize_t my_read(int fd, char *ptr)
+{
+    static int read_cnt;
+    static char *read_ptr;
+    static char read_buf[MAXLINE];
+
+    if (read_cnt <= 0)
+    {
+    again:
+        if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0)
+        {
+            if (errno == EINTR)
+                goto again;
+            return(-1);
+        }
+        else if (read_cnt == 0)
+            return(0);
+        read_ptr = read_buf;
+    }
+    
+    read_cnt--;
+    *ptr = *read_ptr++;
+    return(1);
+}
+
+ssize_t readline(int fd, void *vptr, size_t maxlen)
+{
+    ssize_t n, rc;
+    char c, *ptr;
+
+    ptr = (char*)vptr;
+    for (n = 1; n < maxlen; n++)
+    {
+        if ((rc = my_read(fd, &c)) == 1)
+        {
+            *ptr++ = c;
+            if (c == '\n')
+                break;  /* newline is stored, like fgets() */
+        }
+        else if (rc == 0)
+        {
+            *ptr = 0;
+            return(n - 1);  /* EOF, n - 1 bytes were read */
+        }
+        else
+            return(-1);     /* error, errno set by read() */
+    }
+
+    *ptr = 0;   /* null terminate like fgets() */
+    return(n);
+}
+
+ssize_t Readline(int fd, void *ptr, size_t maxlen)
+{
+    ssize_t n;
+
+    if ( (n = readline(fd, ptr, maxlen)) < 0)
+        err_sys("readline error");
+
+    return(n);
+}
+
+void str_cli(FILE *fp, int sockfd)
+{
+    char sendline[MAXLINE], recvline[MAXLINE];
+
+    while (Fgets(sendline, MAXLINE, fp) != NULL)
+    {
+        Writen(sockfd, sendline, strlen(sendline));
+
+        if (Readline(sockfd, recvline, MAXLINE) == 0)
+            err_quit("strl_cli: server terminated prematurely");
+
+        Fputs(recvline, stdout);
+    }
 }
 
 void str_echo(int sockFd)
@@ -124,6 +205,51 @@ again:
     return(n);
 }
 
+void Connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+    if (connect(fd, sa, salen) < 0)
+        err_sys("connect error");
+}
+
+const char* Inet_ntop(int family, const void *addrptr, char *strptr, size_t len)
+{
+    const char* ptr;
+
+    if (strptr == NULL) /* check for old code */
+        err_quit("NULL 3rd argument to inet_ntop");
+
+    if ((ptr = inet_ntop(family, addrptr, strptr, len)) == NULL)
+        err_sys("inet_ntop error");     /* sets errno */
+
+    return(ptr);
+}
+
+void Inet_pton(int family, const char *strptr, void *addrptr)
+{
+    int n;
+
+    if ((n = inet_pton(family, strptr, addrptr)) < 0)
+        err_sys("inet_pton error for %s", strptr);  /* errno set */
+    else if (n == 0)
+        err_quit("inet_pton error for %s", strptr); /* errno not set */
+}
+
+char* Fgets(char *ptr, int n, FILE *stream)
+{
+    char* rptr;
+
+    if ((rptr = fgets(ptr, n, stream)) == NULL && ferror(stream))
+        err_sys("fgets error");
+
+    return (rptr);
+}
+
+void Fputs(const char *ptr, FILE *stream)
+{
+    if (fputs(ptr, stream) == EOF)
+        err_sys("fputs error");
+}
+
 /* Print message and return to caller
  * Caller specifies "errnoflag" and "level" */
 
@@ -167,6 +293,19 @@ void err_sys(const char *fmt, ...)
     
     va_start(ap, fmt);
     err_doit(1, LOG_ERR, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
+/* Fatal error unrelated to system call
+ * Print message and terminate */
+
+void err_quit(const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start(ap, fmt);
+    err_doit(0, LOG_ERR, fmt, ap);
     va_end(ap);
     exit(1);
 }
