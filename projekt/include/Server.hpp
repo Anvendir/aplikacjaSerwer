365f0f0b07764d3bd8de79931618051c96e98ff5
Rafal Kobak <rafal90gg@gmail.com>
2016-02-17 22:18:45 +0100

    Adding empty Server classes

---
+++ projekt/include/Server.hpp
@@ -0,0 +1,7 @@
+#include "IServer.hpp"
+
+class Server : public IServer
+{
+public:
+    Server();
+};
---
+++ projekt/unitTest/test_modules/ServerTestSuite.cpp
@@ -0,0 +1,18 @@
+#include "Server.hpp"
+
+#include "gtest/gtest.h"
+
+class ServerTestSuite : public ::testing::Test
+{
+    ServerTestSuite()
+    {
+
+    }
+
+    Server m_sut;
+};
+
+TEST(ServerTestSuite, fillme)
+{
+
+}
